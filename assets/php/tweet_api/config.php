92e69ee03b19e6b61e3da5ddb41fc5cf9f5a7384
cameron-bret <c.bret@whiskerhead.com>
2020-08-12 08:02:50 -0400

    added test stream

---
+++ assets/php/tweet_api/config.php
@@ -0,0 +1,13 @@
+<?php
+    /**
+     * Your Twitter App Info
+	 See Docs for informeation
+     */
+    
+    // Consumer Key
+    define('CONSUMER_KEY', 'place_you_CONSUMER_KEY');
+    define('CONSUMER_SECRET', 'Place_your_CONSUMER_SECRET');
+
+    // User Access Token
+    define('ACCESS_TOKEN', 'Place_your_ACCESS_TOKEN');
+    define('ACCESS_SECRET', 'Place_your_ACCESS_SECRET');
